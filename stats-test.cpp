1f41f5a2c00c587aa57d3d681ba58fcf3db2d3d1
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-05-22 15:46:44 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -2,11 +2,12 @@
 
 #include "catch.hpp"
 #include "stats.h"
-
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-TEST_CASE("reports average, minimum and maximum") {
+TEST_CASE("reports average, minimum and maximum")
+{
     float numberset[] = {1.5, 8.9, 3.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
     struct Stats computedStats = compute_statistics(numberset, setlength);
@@ -16,16 +17,16 @@ TEST_CASE("reports average, minimum and maximum") {
     REQUIRE(abs(computedStats.min - 1.5) < epsilon);
 }
 
-TEST_CASE("average is NaN for empty array") {
-    struct Stats computedStats = compute_statistics(0, 0);
-    //All fields of computedStats (average, max, min) must be
-    //NAN (not-a-number), as defined in math.h
-    
-    //Design the REQUIRE statement here.
-    //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
+TEST_CASE("average is NaN for empty array") 
+{
+    struct Stats computedStats = compute_statistics(0, 0);    
+    REQUIRE(computedStats.average == isinf(NAN));
+    REQUIRE(computedStats.average == isinf(NAN));
+    REQUIRE(computedStats.average == isinf(NAN));
 }
 
-TEST_CASE("raises alerts when max is greater than threshold") {
+TEST_CASE("raises alerts when max is greater than threshold") 
+{
     // create additional .c and .h files
     // containing the emailAlerter, ledAlerter functions
     alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
