27a672bb7698421a86911db57b3060644b604636
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-05-22 15:35:45 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,10 +1,38 @@
 #include "stats.h"
+#include<stdio.h>
+#include<math.h>
+void emailAlerter(){}
+void ledAlerter(){}
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
-    average = 0;
-    min = 0;
-    max = 0;
+    struct stats s;
+    float G.Total=0;
+    if(numberset == 0)
+    {      
+    s.average = NAN;
+    s.min = NAN;
+    s.max = NAN;
+    }
+    else
+    {
+        for(int a=0;i<=setlength;a++)
+        {
+            if(numberset[a] > s.max)
+            {
+                s.max = numberset[a]   
+            }
+            if(numberset[a] < s.min)
+            {
+                s.min = numberset[a]   
+            }
+            G.Total = G.Total + numberset[a];
+            s.average = G.Total/setlength;
+        }
+    }
+    return s;
 };
-
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+int emailAlertCallCount = 1;
+int ledAlertCallCount = 1;
+}
